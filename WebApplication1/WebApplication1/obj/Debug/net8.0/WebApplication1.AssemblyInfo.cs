4e07a3e7a7fc600067f3f09f512a05d123af6abe
João Vitor <joaoaansaloni@gmail.com>
2024-05-14 15:33:38 -0300

    Correção tela InforProd

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff92a89a825726d868a11fc24ab384f7674ee6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3c4d895d833133a035d2baf9b3a4280722400b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
